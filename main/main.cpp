1f1f359f8b5083ae7c4307282da2534274cc9dc6
un01s <96022876+un01s@users.noreply.github.com>
2022-10-03 19:21:06 -0600

    add-m5core2-lib

--- main/main.cpp
+++ main/main.cpp
@@ -1,13 +1,32 @@
 #include "Arduino.h"
+#include "M5Core2.h"
 
 void setup(){
-  Serial.begin(115200);
-  while(!Serial){
-    ; // wait for serial port to connect
-  }
+  disableCore0WDT();
+  disableCore1WDT();
+
+  M5.begin();
+  M5.Lcd.setTextColor(YELLOW);  
+  M5.Lcd.setTextSize(2); 
+  M5.Lcd.setCursor(65, 10);  
+  M5.Lcd.println("Button example");
+  M5.Lcd.setCursor(3, 35);
+  M5.Lcd.println("Press button B for 700ms");
+  M5.Lcd.println("to clear screen.");
+  M5.Lcd.setTextColor(RED);
 }
 
 void loop(){
-  Serial.println("hello");
-  delay(1000);
+  M5.update();
+  if (M5.BtnA.wasReleased() || M5.BtnA.pressedFor(1000, 200)) {
+    M5.Lcd.print('A');
+  } else if (M5.BtnB.wasReleased() || M5.BtnB.pressedFor(1000, 200)) {
+    M5.Lcd.print('B');
+  } else if (M5.BtnC.wasReleased() || M5.BtnC.pressedFor(1000, 200)) {
+    M5.Lcd.print('C');
+  } else if (M5.BtnB.wasReleasefor(700)) {
+    M5.Lcd.clear(WHITE);  
+    M5.Lcd.setCursor(0, 0);
+  }
 }
+
